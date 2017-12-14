361ef5b1dd3a05452267d3d889201b72f3cefd0e
SaivNator <sivertsac@gmail.com>
2017-12-14 23:59:45 +0100

    add

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Simple-Clipping
 Simple library for clipping 2d polygons.
+
+# Dependencies
+http://www.wykobi.com/
---
+++ Simple-Clipping/Simple-Clipping/main.cpp
@@ -0,0 +1,17 @@
+//Author: Sivert Andresen Cubedo
+
+#include <iostream>
+
+#include <wykobi.hpp>
+
+int main() {
+
+	wykobi::point2d<float> p1 = wykobi::make_point(100.f, 100.f);
+
+	std::cout << "Hello world\n";
+
+
+
+	return EXIT_SUCCESS;
+}
+
